acd9b524f657d1cf4afeb8f79ef55be29ac674a8
Makemoneyonlinejr <ghostyahaya@gmail.com>
2026-01-23 13:20:29 +0200

    Update tap.php

--- tap.php
+++ tap.php
@@ -4,3 +4,5 @@ $g=floatval($_GET['g']);
 $conn->query("UPDATE users SET balance=balance+$g WHERE id=".$_SESSION['user']);
 }
 header("Location: dashboard.php");
+
+<p>💰 Solde: <strong>$<?= number_format($u['balance'],2) ?></strong></p>
