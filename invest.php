c5fe90bcec58e86ae4f46759c64095a3fad6a654
Makemoneyonlinejr <ghostyahaya@gmail.com>
2026-01-23 13:52:00 +0200

    invest.php

---
+++ invest.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="style.css">
+<div class="container">
+<h2>🚀 Booster tes gains</h2>
+<p>Pour investir et multiplier tes gains :</p>
+<p>📞 Contacte l’admin :</p>
+<h3 style="color:#0f0;">+243987466797</h3>
+<p>(Numéro admin pour paiement & activation)</p>
+<a href="dashboard.php">⬅ Retour</a>
+</div>
